ac794532cf692a7dad1acdc30c719bc0f02f0819
Anton Wyrowski <anton.wyrowski@gmail.com>
2025-02-28 00:42:59 +0000

    feat: timeline item animation

--- components/ui/timeline-item.tsx
+++ components/ui/timeline-item.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { motion } from 'motion/react';
 import React from 'react';
 
 interface TimelineItemProps {
@@ -7,12 +10,34 @@ interface TimelineItemProps {
 }
 
 function TimelineItem({ title, description, date }: TimelineItemProps) {
+  const fadeUpVariants = {
+    hidden: { opacity: 0, y: 30, filter: 'blur(10px)' },
+    visible: {
+      opacity: 1,
+      y: 0,
+      filter: 'blur(0px)',
+      transition: {
+        duration: 1,
+        delay: 0.5,
+        ease: [0.25, 0.4, 0.25, 1],
+      },
+    },
+  };
+
   return (
-    <div className="rounded-lg shadow-md overflow-hidden border border-white/5 bg-white/5 hover:border-white/10 transition-all duration-300 cursor-pointer p-6">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      variants={fadeUpVariants}
+      viewport={{
+        once: true,
+      }}
+      className="rounded-lg shadow-md overflow-hidden border border-white/5 bg-white/5 hover:border-white/10 cursor-pointer p-6"
+    >
       <p className="text-white/60 text-xs">{date}</p>
       <h3 className="text-xl font-bold my-2">{title}</h3>
       <p className="text-white/60">{description}</p>
-    </div>
+    </motion.div>
   );
 }
 
